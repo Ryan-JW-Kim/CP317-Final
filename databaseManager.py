1bd15902d7d7a10b03bc0b9eb05cdd30a826f075
Ryan Kim <43964171+Ryan-JW-Kim@users.noreply.github.com>
2022-04-05 14:59:03 -0400

    Update databaseManager.py

--- databaseManager.py
+++ databaseManager.py
@@ -1,5 +1,4 @@
-
-from types import NoneType
+import random
 import mysql.connector
 import sys
 
@@ -8,6 +7,7 @@ class Cursor:
 		pass
 
 	def execute(self, operation, params=None, multi=False):
+		print(f"	Fake execution complete.... query -> {operation}")
 		return True
 
 class Connection:
@@ -18,16 +18,18 @@ class Database:
 
 	def __init__(self, host, user, password, state="Testing"):
 
-		h = host
-		u = user
-		p = password
+		self.host = host
+		self.user = user
+		self.password = password
+
+		self.state = state
 
 		if state == "Testing":
-			self.connection = Connection(h, u, p)
+			self.connection = Connection(self.host, self.user, self.password)
 			self.cursor = Cursor()
 
 		elif state == "Production":
-			self.connection = mysql.connector.connect(h, u, p)
+			self.connection = mysql.connector.connect(self.host, self.user, self.password)
 			self.cursor = self.connection.cursor()
 
 		else:
@@ -46,21 +48,25 @@ class Database:
 			"yPos": None,
 			"element_id": None
 			}
-		
+
+		query = f"SELECT * FROM database.map WHERE ELEMENT_ID=\'{element_id}\'"
+
+		if self.state == "Testing":
+
+			mapElementDict["type"] = "test_type"
+			mapElementDict["image_path"] = "image.jpeg"
+			mapElementDict["xPos"] = "0"
+			mapElementDict["yPos"] = "0"
+			mapElementDict["element_id"] = str(element_id)
+
+			self.execute_query(query)
+
+			mapElementDict["query"] = query
+
+		else:
+			print(f"Error, calling features unimplemented for the current database state attribute....")
+
 		return mapElementDict
-
-	def get_appointments_by_staff(self, staff_name: str) -> list: # returns a list of dictionaries
-		pass
-
-	def get_appointments_by_student(self, student_name: str) -> list: # returns a list of dictionaries
-		"""
-		This method is called when filling a list GUI element, using this element a python list filled with dictionaries
-		is returned in the below format. given a student name many appointments (or none) may be returned
-
-		Consider methods of the type where a list is returned as a reoccuring call to its get_by_id counterpart
-		the only difference being that a list of such dict objects are returned, with the exact same shape.
-		"""
-		pass
 
 	def get_appointment_by_id(self, appointment_id: str) -> dict:
 		"""
@@ -72,9 +78,32 @@ class Database:
 			"event_name": None,
 			"created_at": None,
 			"event_date": None,
+			"related_student": None,
+			"related_staffer": None,
 			"supplementary_doc_path": None,
-			"event_description": None	
+			"event_description": None,
+			"appointment_id": None
 			}
+
+		query = f"SELECT * FROM database.appointments WHERE APPOINTMENT_ID=\'{appointment_id}\'"
+
+		if self.state == "Testing":
+
+			appointmentDict["event_name"] = "test_appointment_name"
+			appointmentDict["created_at"] = "DD-MM-YY-HH-MM"
+			appointmentDict["event_date"] = "DD-MM-YY-HH-MM"
+			appointmentDict["related_student"] = "test_student"
+			appointmentDict["related_staffer"] = "test_staffer"
+			appointmentDict["supplementary_doc_path"] = "spreadsheat.csv"
+			appointmentDict["event_description"] = "test_appointment_description"
+			appointmentDict["appointment_id"] = str(appointment_id)
+
+			self.execute_query(query)
+
+			appointmentDict["query"] = query
+
+		else:
+			print(f"Error, calling feature unimplemented for the current database state attribute....")
 
 		return appointmentDict
 
@@ -85,13 +114,117 @@ class Database:
 			"company": None,
 			"posting_type": None,
 			"posting_description": None,
-			"application_portal_link": None
+			"application_portal_link": None,
+			"job_posting_id": None
 			}
+
+		query = f"SELECT * FROM database.jobs WHERE JOB_ID=\'{job_posting_id}\'"
+
+		if self.state == "Testing":
+
+			jobDict["job_title"] = "test_job_title"
+			jobDict["company"] = "test_company_name"
+			jobDict["posting_type"] = "test_job_type"
+			jobDict["posting_description"] = "test_job_description"
+			jobDict["application_portal_link"] = "www.indeed.com/jobPosting/fakeID"
+			jobDict["job_posting_id"] = str(job_posting_id)
+
+			self.execute_query(query)
+
+			jobDict["query"] = query
+
+		else:
+			print(f"Error, calling feature unimplemented for the current database state attribute....")
 
 		return jobDict
 
+	def get_appointments_by_staff(self, staff_name: str) -> list: # returns a list of dictionaries
+
+		appointments = []
+
+		query = f"SELECT * FROM database.appointments WHERE RELATED_STAFFER=\'{staff_name}\'"
+
+		if self.state == "Testing":
+	
+			for appointment in range(random.randint(5)):
+
+				appointmentDict = self.get_appointment_by_id(appointment)
+				appointmentDict["query"] = query
+				appointments.append(appointmentDict)
+
+			self.execute_query(query)			
+
+		else:
+			print(f"Error, calling features unimplemented for the current database state attribute.....")
+
+		return appointments
+
+	def get_appointments_by_student(self, student_name: str) -> list: # returns a list of dictionaries
+		"""
+		This method is called when filling a list GUI element, using this element a python list filled with dictionaries
+		is returned in the below format. given a student name many appointments (or none) may be returned
+
+		Consider methods of the type where a list is returned as a reoccuring call to its get_by_id counterpart
+		the only difference being that a list of such dict objects are returned, with the exact same shape.
+		"""
+		
+		appointments = []
+
+		query = f"SELECT * FROM database.appointments WHERE RELATED_STUDENT=\'{student_name}\'"
+
+		if self.state == "Testing":
+
+			for appointment in range(random.randint(5)):
+
+				appointment = self.get_appointment_by_id(appointment)
+				appointment["query"] = query
+				appointments.append(appointment)
+
+			self.execute_query(query)
+
+		else:
+			print(f"Error, calling features unimplemented for the current database state attribute.....")
+
+		return appointments
+
 	def get_jobs_by_company(self, company_name: str) -> list: # returns a list of dictionaries
-		pass
+
+		jobs = []	
+
+		query = f"SELECT * FROM database.jobs WHERE COMPANY=\'{company_name}\'"
+
+		if self.state == "Testing":
+			
+			for job in range(random.randint(5)):
+
+				jobDict = self.get_job_by_id(job)
+				jobDict["query"] = query
+				jobs.append(jobDict)
+
+			self.execute_query(query)
+
+		else:
+			print(f"Error, calling feature unimplemented for the current database state attribute.....")
+
+		return jobs
 
 	def get_jobs_by_type(self, job_posting_type: str) -> list: # returns a list of dictionaries
-		pass
+		
+		jobs = []
+
+		query = f"SELECT * FROM database.jobs WHERE POSTING_TYPE=\'{job_posting_type}\'"
+
+		if self.state == "Testing":
+
+			for job in range(random.randint(5)):
+
+				jobDict = self.get_job_by_id(job)
+				jobDict["query"] = query
+				jobs.append(jobDict)
+
+			self.execute_query(query)
+
+		else:
+			print(f"Error, calling feature unimplemnted for the current database state attribute......")
+
+		return jobs
